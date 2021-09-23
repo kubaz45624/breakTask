dfbd4f383b6a7bd3c5860e7db6e75e5a1962eeae
kubaz45624 <jakub.zielinski45624@gmail.com>
2021-09-23 16:25:15 +0200

    adding override toString and constructor with one parameter

--- src/main/java/breaks/PayoutBreak.java
+++ src/main/java/breaks/PayoutBreak.java
@@ -2,12 +2,20 @@ package breaks;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class PayoutBreak implements Comparable<PayoutBreak> {
     private LocalTime payoutBreakTime;
     private LocalTime requiredWorkTime;
     private LocalDate validFrom;
     private LocalDate validTo;
+
+    public PayoutBreak(LocalTime payoutBreakTime) {
+        this.payoutBreakTime = payoutBreakTime;
+        this.requiredWorkTime = LocalTime.MIN;
+        this.validFrom = LocalDate.MIN;
+        this.validTo = LocalDate.MAX;
+    }
 
     public PayoutBreak(LocalTime payoutBreakTime, LocalTime requiredWorkTime, LocalDate validFrom, LocalDate validTo) {
         this.payoutBreakTime = payoutBreakTime;
@@ -46,6 +54,25 @@ public class PayoutBreak implements Comparable<PayoutBreak> {
 
     public void setValidTo(LocalDate validTo) {
         this.validTo = validTo;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        StringBuilder sb = new StringBuilder();
+        sb.append(payoutBreakTime).append(" ").append(requiredWorkTime).append(" ");
+        if (validFrom.isEqual(LocalDate.MIN)) {
+            sb.append("          ");
+        } else {
+            sb.append(validFrom.format(dateTimeFormatter));
+        }
+        sb.append(" ");
+        if (validTo.isEqual(LocalDate.MAX)) {
+            sb.append("          ");
+        } else {
+            sb.append(validTo.format(dateTimeFormatter));
+        }
+        return sb.toString();
     }
 
     @Override
